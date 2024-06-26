4eae0d647ee2ea34b6d85adaff3efbe3bc26d574
vickybedrova <vikibedrova123@gmail.com>
2024-06-27 02:14:29 +0300

    example product page

--- src/product_detail.php
+++ src/product_detail.php
@@ -1,43 +1,45 @@
-<?php
-
-session_start();
-
-include 'connection.php';
-$product_id = $_GET['id']; 
-
-$sql = "SELECT * FROM product WHERE id = $product_id";
-$result = $conn->query($sql);
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Product Detail</title>
+    <link rel="stylesheet" href="style.css">
+    <?php include 'navigation-bar.php'; ?>
 </head>
 <body>
-    <?php if ($result->num_rows > 0) {
-      $product = $result->fetch_assoc(); ?>
-        <h1><?php echo $product['name']; ?></h1>
-        <img src="<?php echo $product['image']; // Assuming an image field exists ?>" alt="<?php echo $product['name']; ?>">
-        <p><?php echo $product['description']; ?></p>
-        <p>Price: $<?php echo number_format($product['price'], 2); ?></p>
+    <div class="container">
+       <?php
 
-        <?php if (isset($_SESSION['user_id'])) { // Check if user is logged in ?>
-          <form action="add_to_cart.php" method="post">
-            <label for="quantity">Quantity:</label>
-            <input type="number" name="quantity" id="quantity" min="1" value="1">
-            <input type="hidden" name="product_id" value="<?php echo $product_id; ?>">
-            <input type="submit" value="Add to Cart">
-          </form>
-        <?php } else { ?>
-          <p>Please log in to add to cart.</p>
-        <?php } ?>
+        /*$product = [
+            'id' => 1,
+            'image' => 'path_to_image.jpg',
+            'title' => 'Product Title',
+            'description' => 'Product Description',
+            'price' => 19.99
+        ];
+        ?>*/
 
-      <?php } else { ?>
-        <p>Product not found!</p>
-      <?php } ?>
-</body>
+      /*  <div class="product-detail">
+            <div class="product-image">
+                <img src="<?php echo $product['image']; ?>" alt="Product Image">
+            </div>
+            <div class="product-info">
+                <h2><?php echo $product['title']; ?></h2>
+                <p><?php echo $product['description']; ?></p>
+                <p>Price: $<?php echo number_format($product['price'], 2); ?></p>
+                
+                <!-- Form for adding to cart -->
+                <form action="add_to_cart.php" method="post">
+                    <input type="hidden" name="product_id" value="<?php echo $product['id']; ?>">
+                    
+                    <label for="quantity">Quantity:</label>
+                    <input type="number" id="quantity" name="quantity" value="1" min="1" max="10">
+                    
+                    <button type="submit">Add to Cart</button>
+                </form>
+            </div>
+        </div>*/
+        ?>
+    </div>
+  </body>
 </html>
-
