4eebdabe212eadac8de8efe0237848965926909c
nnikolov01 <n.p.nikolov01@gmail.com>
2024-06-27 03:16:23 +0300

    Navbar update

--- src/navigation-bar.php
+++ src/navigation-bar.php
@@ -5,6 +5,15 @@
         <li><a href="products.php">Browse Products</a></li>
         <li><a href="profile.php">View Profile</a></li>
         <li><a href="cart.php">View Cart</a></li>
-        <li><a href="logout.php">Logout</a></li>
+        <li>
+            <?php
+            session_start();
+            $username = isset($_SESSION['Email']) ? $_SESSION['Email'] : '';
+            if ($username) {
+                echo '<span>Logged in as: ' . htmlspecialchars($username) . '!</span>';
+            }
+            ?>
+            <a href="logout.php">Logout</a>
+        </li>
     </ul>
 </nav>
